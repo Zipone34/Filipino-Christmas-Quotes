c9e52ab26087acf4b6fc0616760adaa4b6567095
Zipone34 <115150262+Zipone34@users.noreply.github.com>
2023-12-19 22:39:44 +0800

    Add files via upload
    I created this using html, css, Javascript...

---
+++ ChristmasQuotes.js
@@ -0,0 +1,12 @@
+ function myFunction() {
+    document.getElementById("demo").innerHTML = "para may pagkakataon pa akong KUMPLETUHIN mo.♥";
+  }
+  function Regalo() {
+    document.getElementById("hu").innerHTML = "………….SAYO.♥";
+  }
+  function Pasko() {
+    document.getElementById("ho").innerHTML = "ay manatili pa rin sa buhay ko..♥";
+  }
+  function Bigayan() {
+    document.getElementById("he").innerHTML = "Sana mahanap muna yong itinadhana para sayo♥♥♥";
+  }
